7c804b8ba51fe4e27f8ded447141c6f69dcf409e
Douglas Dunn <douglas.dunn@outlook.com>
2017-01-04 17:58:56 -0800

    initialized project repository

---
+++ app/components/Main.jsx
@@ -0,0 +1,18 @@
+var React = require("react");
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div>
+          <div>
+            <p>Main.jsx Rendered</p>
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Main;
